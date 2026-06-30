8a9ca20148e4752a01cf6542cdc946cde2c0c7dc
TCHANILEY <gtchaniley@gmail.com>
2026-06-30 14:59:12 +0000

    Initial DHIS2 SQL Reports setup

--- d2.config.js
+++ d2.config.js
@@ -1,12 +1,28 @@
 /** @type {import('@dhis2/cli-app-scripts').D2Config} */
+// const config = {
+//     type: 'app',
+
+//     entryPoints: {
+//         app: './src/App.jsx',
+//     },
+
+//     direction: 'auto',
+// }
+
+// module.exports = config
 const config = {
     type: 'app',
+    name: 'dhis2-sql-reports',
 
     entryPoints: {
         app: './src/App.jsx',
     },
 
-    direction: 'auto',
+    dataStore: true,
+
+    auth: {
+        baseUrl: 'https://palu.togodhis2.org/mid',
+    },
 }
 
-module.exports = config
+export default config
